1c53dd432f18eb4605bb673f1d26ad76076c4b13
patrictobola <tobola92@gmail.com>
2023-08-22 15:52:04 +0200

    done controller and model

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        dd($movies);
+        return view('home');
+    }
 }
