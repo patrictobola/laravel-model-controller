d46466574e017cbaf469a75dc93d7037edc8c8dd
patrictobola <tobola92@gmail.com>
2023-08-22 16:12:24 +0200

    done a card in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies from DB</title>
-        <!-- Fonts -->
+    <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     @vite('resources/js/app.js')
 </head>
+
 <body>
-    
+    <main>
+        <div class="container">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
+
 </html>
